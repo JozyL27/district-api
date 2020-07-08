b27790e0de440ec39fb790a598e9b39db4d1a668
Jose <jozylopez27@gmail.com>
2020-07-08 17:47:24 -0500

    added middleware folder and moved errorhandler there

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV} = require('./config')
+const errorHandler = require('./middleware/error-handler')
 
 const app = express()
 
@@ -16,19 +17,11 @@ app.use(helmet())
 app.use(cors())
 
 
+// app.use('/api/user')
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.log(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler)
 
 module.exports = app
