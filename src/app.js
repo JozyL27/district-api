ea88f72019042c993ddf5cc2ae60375061bfc1db
Jose <jozylopez27@gmail.com>
2020-07-10 22:23:28 -0500

    added user endpoint to app file

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV} = require('./config')
 const errorHandler = require('./middleware/error-handler')
+const UserRouter = require('./user/user-router')
 
 const app = express()
 
@@ -17,7 +18,7 @@ app.use(helmet())
 app.use(cors())
 
 
-// app.use('/api/user')
+app.use('/api/user', UserRouter)
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
