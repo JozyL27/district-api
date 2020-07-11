2ae6bbb39eba80948f187a8bd97bf98c039a48ed
Jose <jozylopez27@gmail.com>
2020-07-10 22:21:04 -0500

    added user endpoint and updated config / server files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_URL: process.env.DB_URL
+        || 'postgresql://postgres@localhost/district',
+    JWT_SECRET: process.env.JWT_SECRET || 'super-super-super-super-secret',
 }
---
+++ src/middleware/jwt-auth.js
@@ -0,0 +1,1 @@
+const { JsonWebTokenError } = require('jsonwebtoken')
