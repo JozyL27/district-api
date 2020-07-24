f6c8d25884e3898ae0a0e5ebd38aaa48e8b9a044
Jose <jozylopez27@gmail.com>
2020-07-24 13:19:52 -0500

    added a get endpoint for comments

--- src/articles/article-router.js
+++ src/articles/article-router.js
@@ -17,5 +17,53 @@ ArticlesRouter
         }
     })
 
+ArticlesRouter
+    .route('/latest')
+    .get(jsonBodyParser, async (req, res, next) => {
+        const { page } = req.body
+        try {
+            const articles = await ArticlesService.getMostRecentArticles(
+                req.app.get('db'),
+                page
+            )
+            res.status(200).json(articles)
+        } catch(error) {
+            next(error)
+        }
+    })
+
+ArticlesRouter
+    .route('/')
+    .get(jsonBodyParser, async (req, res, next) => {
+        const { page } = req.body
+        try {
+            const articles = await ArticlesService.getAllArticles(
+                req.app.get('db'),
+                page
+            )
+            res.status(200).json(articles)
+        } catch(error) {
+            next(error)
+        }
+    })
+
+ArticlesRouter
+    .route('/:articleId/comments')
+    .get(jsonBodyParser, async (req, res, next) => {
+        const { articleId } = req.params
+        const { page } = req.body
+        try {
+            const comments = await ArticlesService.getArticleComments(
+                req.app.get('db'),
+                articleId,
+                page
+            )
+            res.status(200).json(comments)
+        } catch(error) {
+            next(error)
+        }
+    })
+
+
 
 module.exports = ArticlesRouter
--- src/articles/articles-service.js
+++ src/articles/articles-service.js
@@ -2,6 +2,40 @@ const ArticlesService = {
     getCategories(db) {
         return db.raw('SELECT unnest(enum_range(NULL::article_category))::text AS category')
     },
+    getMostRecentArticles(db, page = 1) {
+        const articlesPerPage = 10
+        const offset = articlesPerPage * (page - 1)
+
+        return db('district_articles')
+        .select('*')
+        .orderBy('date_published', 'desc')
+        .limit(articlesPerPage)
+        .offset(offset)
+    },
+    getArticleComments(db, articleId, page = 1) {
+        const commentsPerpage = 15
+        const offset = commentsPerpage * (page - 1)
+
+        return db('district_comments')
+        .select('text', 'date_commented', 
+        'article_id', 'user_id', 
+        'username', 'avatar', 
+        'district_comments.id')
+        .orderBy('date_commented', 'desc')
+        .innerJoin('district_users', 'district_comments.user_id', 'district_users.id')
+        .where('district_comments.article_id', articleId)
+        .limit(commentsPerpage)
+        .offset(offset)
+    },
+    getAllArticles(db, page = 1) {
+        const articlesPerPage = 10
+        const offset = articlesPerPage * (page - 1)
+
+        return db('district_articles')
+        .select('*')
+        .limit(articlesPerPage)
+        .offset(offset)
+    },
 }
 
 module.exports = ArticlesService
